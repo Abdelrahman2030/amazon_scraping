7a42c7549cbc865a481ea9269fc04b8fe5645561
abdelrahman-elgandor <abdelrahmanelgandor230@gmail.com>
2023-10-26 11:31:20 +0200

    connected jumia code to the collective code

--- the_final_code/collective_code.py
+++ the_final_code/collective_code.py
@@ -5,15 +5,28 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+start_time = time.time()
 #i will need to upload the other python files for
 #sutra 
 #Jumia
 
-import sutra_function
+import sutra_function #to import thr function of sutra
 
 keyword = input("Enter your keyword: ") #The search key words
 keyword = keyword.replace(" ", "+")
+print(keyword)
 
 sutra_df = sutra_function.main_sutra(keyword)
-#No we have connected with sutra code, and have two things, first csv file
+#Now we have connected with sutra code, and have two things, first csv file
 #seconde thinf sutra_df
+
+
+
+import jumia_function
+
+jumia_df = jumia_function.jumia_main(keyword)
+
+end_time = time.time()
+
+time_taken = (end_time - start_time) / 60
+print("the total time taken of the collective code is {} minutes".format(time_taken))
